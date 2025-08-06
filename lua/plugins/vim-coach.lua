6ddd23daba00842fd5b2ed58d9bb43ed9a31781f
Morgan Mastrocinque <morganmastrocinque@gmail.com>
2025-08-05 22:43:53 -0400

    add stuff

--- lua/plugins/vim-coach.lua
+++ lua/plugins/vim-coach.lua
@@ -1,13 +1,13 @@
 return {
-  "shahshlok/vim-coach.nvim",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    require("vim-coach").setup()
-  end,
-  keys = {
-    { "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
-  },
+	"shahshlok/vim-coach.nvim",
+	dependencies = {
+		"folke/snacks.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("vim-coach").setup()
+	end,
+	keys = {
+		{ "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
+	},
 }
